39cef75eeb2b258fb1292a6ec06c0271380e0888
jmmarao <jmmarao@gmail.com>
2022-11-30 22:39:30 -0300

    Implement activity movie layout

---
+++ app/src/main/java/br/edu/ifsp/moviesmanager/views/MovieActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.ifsp.moviesmanager.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.ifsp.moviesmanager.R
+
+class MovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie)
+    }
+}
