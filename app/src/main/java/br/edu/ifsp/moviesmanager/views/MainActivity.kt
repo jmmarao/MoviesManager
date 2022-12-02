1bc22237b0fac92b0ed3af24338b00be2427ee98
jmmarao <jmmarao@gmail.com>
2022-12-02 20:08:48 -0300

    Refactor and implement main and movie activities. Refactor activity movie layout

--- app/src/main/java/br/edu/ifsp/moviesmanager/views/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/moviesmanager/views/MainActivity.kt
@@ -1,23 +1,110 @@
 package br.edu.ifsp.moviesmanager.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import br.edu.ifsp.moviesmanager.R
+import br.edu.ifsp.moviesmanager.adapters.MovieAdapter
 import br.edu.ifsp.moviesmanager.databinding.ActivityMainBinding
+import br.edu.ifsp.moviesmanager.models.Constants.EXTRA_MOVIE
+import br.edu.ifsp.moviesmanager.models.Constants.VIEW_MOVIE
+import br.edu.ifsp.moviesmanager.models.entities.Movie
 
 class MainActivity : AppCompatActivity() {
     private val activityMainBinding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private val movieList: MutableList<Movie> = mutableListOf()
+    private lateinit var movieAdapter: MovieAdapter
+    private lateinit var movieActivityResultLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainBinding.root)
+
+        movieAdapter = MovieAdapter(this, movieList)
+        activityMainBinding.moviesListView.adapter = movieAdapter
+
+        movieActivityResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+        ) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val movie = result.data?.getParcelableExtra<Movie>(EXTRA_MOVIE)
+
+                movie?.let { _movie ->
+                    val position = movieList.indexOfFirst { it.id == _movie.id }
+                    if (position != -1) {
+                        movieList[position] = _movie
+                    } else {
+                        movieList.add(_movie)
+                    }
+                    movieAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+        registerForContextMenu(activityMainBinding.moviesListView)
+
+        activityMainBinding.moviesListView.onItemClickListener =
+            AdapterView.OnItemClickListener { _, _, position, _ ->
+                val movie = movieList[position]
+                val movieIntent = Intent(this@MainActivity, MovieActivity::class.java)
+                movieIntent.putExtra(EXTRA_MOVIE, movie)
+                movieIntent.putExtra(VIEW_MOVIE, true)
+                startActivity(movieIntent)
+            }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.addMovieMi -> {
+                movieActivityResultLauncher.launch(Intent(this, MovieActivity::class.java))
+                true
+            }
+            else -> {
+                false
+            }
+        }
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        menuInflater.inflate(R.menu.context_main_menu, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val position = (item.menuInfo as AdapterView.AdapterContextMenuInfo).position
+        return when (item.itemId) {
+            R.id.editMovieMi -> {
+                val bill = movieList[position]
+                val movieIntent = Intent(this, MovieActivity::class.java)
+                movieIntent.putExtra(EXTRA_MOVIE, bill)
+                movieIntent.putExtra(VIEW_MOVIE, false)
+                movieActivityResultLauncher.launch(movieIntent)
+                true
+            }
+            R.id.removeMovieMi -> {
+                movieList.removeAt(position)
+                movieAdapter.notifyDataSetChanged()
+                true
+            }
+            else -> {
+                false
+            }
+        }
     }
 }
--- app/src/main/java/br/edu/ifsp/moviesmanager/views/MovieActivity.kt
+++ app/src/main/java/br/edu/ifsp/moviesmanager/views/MovieActivity.kt
@@ -1,12 +1,68 @@
 package br.edu.ifsp.moviesmanager.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.edu.ifsp.moviesmanager.R
+import android.view.View
+import br.edu.ifsp.moviesmanager.databinding.ActivityMovieBinding
+import br.edu.ifsp.moviesmanager.models.Constants.EXTRA_MOVIE
+import br.edu.ifsp.moviesmanager.models.Constants.VIEW_MOVIE
+import br.edu.ifsp.moviesmanager.models.entities.Movie
+import kotlin.random.Random
 
 class MovieActivity : AppCompatActivity() {
+    private val activityMovieBinding: ActivityMovieBinding by lazy {
+        ActivityMovieBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie)
+        setContentView(activityMovieBinding.root)
+        val receivedMovie = intent.getParcelableExtra<Movie>(EXTRA_MOVIE)
+
+        receivedMovie?.let { _receivedMovie ->
+            with(activityMovieBinding) {
+                with(_receivedMovie) {
+                    movieEditText.setText(name)
+                    releaseYearEditText.setText(releaseYear.toString())
+                    producerEditText.setText(producer)
+                    durationEditText.setText(duration.toString())
+                    genreSpinner.selectedItem.toString()
+                    radioButtonNo.isChecked.toString()
+                    scoreEditText.setText(score.toString())
+                }
+            }
+        }
+
+        val viewMovie = intent.getBooleanExtra(VIEW_MOVIE, false)
+        if (viewMovie) {
+            activityMovieBinding.movieEditText.isEnabled = false
+            activityMovieBinding.releaseYearEditText.isEnabled = false
+            activityMovieBinding.producerEditText.isEnabled = false
+            activityMovieBinding.durationEditText.isEnabled = false
+            activityMovieBinding.genreSpinner.isEnabled = false
+            activityMovieBinding.radioButtonYes.isEnabled = false
+            activityMovieBinding.radioButtonNo.isEnabled = false
+            activityMovieBinding.scoreEditText.isEnabled = false
+            activityMovieBinding.saveButton.visibility = View.GONE
+        }
+
+        activityMovieBinding.saveButton.setOnClickListener {
+            val movie = Movie(
+                id = receivedMovie?.id ?: Random(System.currentTimeMillis()).nextInt(),
+                name = activityMovieBinding.movieEditText.text.toString(),
+                releaseYear = activityMovieBinding.releaseYearEditText.text.toString().toInt(),
+                producer = activityMovieBinding.producerEditText.text.toString(),
+                duration = activityMovieBinding.durationEditText.text.toString().toInt(),
+                genre = activityMovieBinding.genreSpinner.selectedItem.toString(),
+                watched = activityMovieBinding.radioButtonNo.isChecked,
+                score = activityMovieBinding.scoreEditText.text.toString().toInt()
+            )
+
+            val resultIntent = Intent()
+            resultIntent.putExtra(EXTRA_MOVIE, movie)
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
     }
 }
